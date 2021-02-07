80e5596f381378af520c0fe93776ed33fdf4e5d1
[Simon Que] <[ques86225@gmail.com]>
2021-02-07 18:46:51 +0800

    added createOpenLogFile, added code to create a new log file every new day, moved the file creation code into the main loop

--- weather_logger.py
+++ weather_logger.py
@@ -3,10 +3,13 @@ import Adafruit_BMP.BMP085 as BMP085
 from time import sleep
 import csv
 import os
-import datetime
+from datetime import datetime, date, timedelta
 
-debug = False # setting debug to True will print data
+debug = True # setting debug to True will print data
 delay = 4 # logging delay
+# specify weather log path and filename
+path = "/home/pi/weather_logs/"
+filenameprefix = "weather_log"
 
 # check if a file exists
 def exists(filename):
@@ -20,28 +23,29 @@ def exists(filename):
 
 # generate csv filename
 # filename + 4 digit number with leading zeroes starting from 0000 + ".csv"
-def generateFileName(filenameprefix0):
-    dnow = datetime.date.today()
-    filepath = filenameprefix0 + dnow.strftime('%m%d%Y') + '.csv'
+def generateFileName():
+    global filenameprefix
+    dnow = date.today()
+    filename = filenameprefix + dnow.strftime('%m%d%Y') + '.csv'
+    return filename
+
+
+# create the complete file path of the log file
+def generateFilePath():
+    global path
+    filepath = path + generateFileName()
     return filepath
 
-# open a new csv file and write the csv file header
-def newLogFile():
-    global filepath 
-
+# create a new csv file and write the csv file header
+def createOpenLogFile():
+    global filepath
+    filepath = generateFilePath()
     csvfile = open(filepath, 'a', newline='')
     writer = csv.writer(csvfile, delimiter=',')
     if os.stat(filepath).st_size == 0:
         writer.writerow(["Time", "Humidity", "Temperature", "BMP_temperature", "Pressure"])
     csvfile.close()
-
-'''
-specify weather log path, filename, and filepath
-'''
-path = "/home/pi/weather_logs/"
-filenameprefix = "weather_log"
-filepath = path + generateFileName(filenameprefix)
-print(filepath)
+    print(filepath)
 
 # try to create the dir for logs if it does not exist
 try:
@@ -50,6 +54,9 @@ except FileExistsError:
     print("dir exists")
     pass
 
+filepath = ""
+dtnow = datetime(1, 1, 1)
+
 # define DHT sensor type
 dsensor = Adafruit_DHT.DHT11
 pin = 4
@@ -57,8 +64,16 @@ sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
 # read sensor and write to log file indefinitely
 while True:
-
     try:
+        # if a new day has started, create a new log file for the day
+        if date.today() > dtnow.date():
+            if debug:
+                if (date.today() - dtnow.date()).days == 1:
+                    print("closing current file and opening a new file")
+                else:
+                    print("Opening file initially")
+            createOpenLogFile()
+            dtnow = datetime.now()
         # read sensors
         humidity, temperature = Adafruit_DHT.read(dsensor, pin)
         bmp_temperature, pressure = sensor.read_temperature(), sensor.read_pressure()
@@ -66,7 +81,7 @@ while True:
         if humidity is not None and temperature is not None and bmp_temperature is not None and pressure is not None:
             csvfile = open(filepath, 'a', newline='')
             writer = csv.writer(csvfile, delimiter=',')
-            dtnow = datetime.datetime.now()
+            dtnow = datetime.now()
             timeStr = dtnow.strftime('%H:%M:%S')
             if debug:
                 print("{} {:0.3f}% {:0.3f}C {:0.3f}C {:0.3f}Pa".format(timeStr, humidity, temperature, bmp_temperature, pressure))
