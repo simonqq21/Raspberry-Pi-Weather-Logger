2754b820e95060b6aca13dc46e49ecf59a201a16
[Simon Que] <[ques86225@gmail.com]>
2021-02-07 17:13:01 +0800

    Added BMP180 barometric pressure sensor, changed file name to reflect the date, added more data to the CSV file.

--- weather_logger.py
+++ weather_logger.py
@@ -1,4 +1,5 @@
 import Adafruit_DHT
+import Adafruit_BMP.BMP085 as BMP085
 from time import sleep
 import csv
 import os
@@ -19,15 +20,9 @@ def exists(filename):
 
 # generate csv filename
 # filename + 4 digit number with leading zeroes starting from 0000 + ".csv"
-def generateFileName(filepathO):
-    filenum = 0
-    exists1 = 1
-    while exists1 == 1:
-        filepath = filepathO
-        filepath += '{:04d}'.format(filenum)
-        filepath += '.csv'
-        filenum += 1
-        exists1 = exists(filepath)
+def generateFileName(filenameprefix0):
+    dnow = datetime.date.today()
+    filepath = filenameprefix0 + dnow.strftime('%m%d%Y') + '.csv'
     return filepath
 
 '''
@@ -35,8 +30,7 @@ specify weather log path, filename, and filepath
 '''
 path = "/home/pi/weather_logs/"
 filenameprefix = "weather_log"
-filepath = path + filenameprefix
-filepath = generateFileName(filepath)
+filepath = path + generateFileName(filenameprefix)
 print(filepath)
 
 # try to create the dir for logs
@@ -47,31 +41,33 @@ except FileExistsError:
     pass
 
 # open csv file and write the csv file header
-csvfile = open(filepath, 'w', newline='')
+csvfile = open(filepath, 'a', newline='')
 writer = csv.writer(csvfile, delimiter=',')
-writer.writerow(["Date", "Time", "Humidity", "Temperature"])
+if os.stat(filepath).st_size == 0:
+    writer.writerow(["Time", "Humidity", "Temperature", "BMP_temperature", "Pressure"])
 csvfile.close()
 
 # define DHT sensor type
 dsensor = Adafruit_DHT.DHT11
 pin = 4
+sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
 # read sensor and write to log file indefinitely
 while True:
     try:
-        # read sensor
+        # read sensors
         humidity, temperature = Adafruit_DHT.read(dsensor, pin)
+        bmp_temperature, pressure = sensor.read_temperature(), sensor.read_pressure()
         # errors may occur due to the precise timing required by the DHT11 sensor
-        if humidity is not None and temperature is not None:
+        if humidity is not None and temperature is not None and bmp_temperature is not None and pressure is not None:
             csvfile = open(filepath, 'a', newline='')
             writer = csv.writer(csvfile, delimiter=',')
             dtnow = datetime.datetime.now()
-            dateStr = dtnow.strftime('%m/%d/%Y')
             timeStr = dtnow.strftime('%H:%M:%S')
             if debug:
-                print("{} {} {:0.3f}% {:0.3f}C".format(dateStr, timeStr, humidity, temperature))
+                print("{} {:0.3f}% {:0.3f}C {:0.3f}C {:0.3f}Pa".format(timeStr, humidity, temperature, bmp_temperature, pressure))
             # write csv data to file
-            writer.writerow([dateStr, timeStr, humidity, temperature])
+            writer.writerow([timeStr, humidity, temperature, bmp_temperature, pressure])
             csvfile.close()
         else:
             if debug:
