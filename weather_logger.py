fb8f7637d00215e56f426ef02ed193a371250278
[Simon Que] <[ques86225@gmail.com]>
2021-02-07 11:31:27 +0800

    added more descriptive comments set the proper path to "/home/pi/weather_logs" set the filename to weather_log<xxxx>.py, where xxxx is a 4 digit zero padded number added KeyboardInterrupt condition to safely close the csv file

--- weather_logger.py
+++ weather_logger.py
@@ -4,8 +4,8 @@ import csv
 import os
 import datetime
 
-debug = False
-delay = 4
+debug = False # setting debug to True will print data
+delay = 4 # logging delay
 
 # check if a file exists
 def exists(filename):
@@ -17,8 +17,8 @@ def exists(filename):
     f.close()
     return 1
 
-# create csv file
-# "dht_log" + 4 digit number with leading zeroes starting from 1 + ".csv"
+# generate csv filename
+# filename + 4 digit number with leading zeroes starting from 0000 + ".csv"
 def generateFileName(filepathO):
     filenum = 0
     exists1 = 1
@@ -30,47 +30,48 @@ def generateFileName(filepathO):
         exists1 = exists(filepath)
     return filepath
 
-path = "/home/pi/dht_logs/"
-filenameprefix = "dht_log"
-filepath = path + generateFileName(filenameprefix)
+'''
+specify weather log path, filename, and filepath
+'''
+path = "/home/pi/weather_logs/"
+filenameprefix = "weather_log"
+filepath = path + filenameprefix
+filepath = generateFileName(filepath)
 print(filepath)
 
 # try to create the dir for logs
 try:
     os.mkdir(path)
 except FileExistsError:
+    print("dir exists")
     pass
 
-# open csv file
-# if exists(filepath) == 0:
-#     csvfile = open(filepath, 'w', newline='')
-#     writer = csv.writer(csvfile, delimiter=',')
-#     writer.writerow(["Date", "Time", "Humidity", "Temperature"])
-#     csvfile.close()
-csvfile = open(filepath, 'a', newline='')
+# open csv file and write the csv file header
+csvfile = open(filepath, 'w', newline='')
 writer = csv.writer(csvfile, delimiter=',')
-if os.stat(filepath).st_size == 0:
-    writer.writerow(["Date", "Time", "Humidity", "Temperature"])
+writer.writerow(["Date", "Time", "Humidity", "Temperature"])
 csvfile.close()
 
 # define DHT sensor type
 dsensor = Adafruit_DHT.DHT11
 pin = 4
-sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
 # read sensor and write to log file indefinitely
 while True:
     try:
+        # read sensor
         humidity, temperature = Adafruit_DHT.read(dsensor, pin)
-        bmp_temperature, pressure = sensor.read_temperature(), sensor.read_pressure()
-        if humidity is not None and temperature is not None and bmp_temperature is not None and pressure is not None:
+        # errors may occur due to the precise timing required by the DHT11 sensor
+        if humidity is not None and temperature is not None:
             csvfile = open(filepath, 'a', newline='')
             writer = csv.writer(csvfile, delimiter=',')
             dtnow = datetime.datetime.now()
+            dateStr = dtnow.strftime('%m/%d/%Y')
             timeStr = dtnow.strftime('%H:%M:%S')
             if debug:
-                print("{} {:0.3f}% {:0.3f}C {:0.3f}C {:0.3f}Pa".format(timeStr, humidity, temperature, bmp_temperature, pressure))
-            writer.writerow([timeStr, humidity, temperature, bmp_temperature, pressure])
+                print("{} {} {:0.3f}% {:0.3f}C".format(dateStr, timeStr, humidity, temperature))
+            # write csv data to file
+            writer.writerow([dateStr, timeStr, humidity, temperature])
             csvfile.close()
         else:
             if debug:
