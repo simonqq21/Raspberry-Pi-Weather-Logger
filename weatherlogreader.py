a552c4df03e95a7ebbf4d51dfb7727c206010e56
[Simon Que] <[ques86225@gmail.com]>
2021-02-11 18:19:07 +0800

    added new features file

--- weatherlogreader.py
+++ weatherlogreader.py
@@ -4,12 +4,7 @@ import csv
 import datetime
 import argparse
 
-# take arguments:
-# date of a datalog file
-# print out the table of temperature and humidity values
-# get the mean and standard deviation of temperature and humidity values
-# save the graph of temperature and humidity over time into an image with statistical measures
-# written on it
+
 parser = argparse.ArgumentParser()
 parser.add_argument("logdate", help="The specific date when the log was taken")
 args = parser.parse_args()
