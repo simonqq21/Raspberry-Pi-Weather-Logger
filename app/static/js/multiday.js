3cfed91853f544dc487873539477ee8ae3011132
Simon Que <ques86225@gmail.com>
2021-06-24 07:42:34 +0800

    added comments and JS UI code to inform and direct user that an operation is still ongoing

--- app/static/js/multiday.js
+++ app/static/js/multiday.js
@@ -1,33 +1,45 @@
 submitDate = function submitDate(e) {
 	e.preventDefault();
+	//~ get date and empty all fields
+	datestartstr = $('#datestart').val();
+	dateendstr = $('#dateend').val();
 	$("#reportfile iframe").attr("src", "");
 	$("#imageplot img").attr("src", "");
 	$("#exported").attr('href', "");
 	$("#aggexported").attr('href', "");
-			
-	datestartstr = $('#datestart').val();
-	dateendstr = $('#dateend').val();
-	datestart = new Date(datestartstr);
-	dateend = new Date(dateendstr);
-	data = {datestart: datestart.toISOString(), dateend: dateend.toISOString()};
-	console.log(data);
-	$.ajax({
-		method: 'GET',
-		url: '/getURLsWithDateRange',
-		data: data,
-		success: function (result, status, xhr) {
-			$('#datestart').val(result.datestart);
-			$('#dateend').val(result.dateend);
-			$("#reportfile iframe").attr("src", result.report_url);
-			$("#imageplot img").attr("src", result.plot_url);
-			$("#exported").attr('href', result.exported_data_url);
-			$("#aggexported").attr('href', result.agg_exported_data_url);
-			console.log(result);
-		}
-	});
+	
+	//~ check if dates are not null
+	if (datestartstr != "" && dateendstr != "") {
+		//~ disable submit button and display info message
+		$("#datessubmit").attr('disabled', true);
+		$("#info").text("please wait, exporting data and generating plots.");
+		//~ convert str to date to send to backend
+		datestart = new Date(datestartstr);
+		dateend = new Date(dateendstr);
+		data = {datestart: datestart.toISOString(), dateend: dateend.toISOString()};
+		console.log(data);
+		//~ ajax call to get URLs and download links from backend
+		$.ajax({
+			method: 'GET',
+			url: '/getURLsWithDateRange',
+			data: data,
+			success: function (result, status, xhr) {
+				$('#datestart').val(result.datestart);
+				$('#dateend').val(result.dateend);
+				$("#reportfile iframe").attr("src", result.report_url);
+				$("#imageplot img").attr("src", result.plot_url);
+				$("#exported").attr('href', result.exported_data_url);
+				$("#aggexported").attr('href', result.agg_exported_data_url);
+				//~ reenable submit button and clear info text
+				$("#info").text("");
+				$("#datessubmit").attr('disabled', false);
+			}
+		});
+	}
 }
 
 $(document).ready(function() {
+	//~ empty all fields when document loaded
 	$('#datestart').val("");
 	$('#dateend').val("");
 	$("#reportfile iframe").attr("src", "");
--- app/static/js/singleday.js
+++ app/static/js/singleday.js
@@ -1,30 +1,41 @@
 submitDate = function submitDate(e) {
 	e.preventDefault();
+	//~ get date and empty all fields
+	date = $('#date').val();
 	$("#reportfile iframe").attr("src", "");
 	$("#imageplot img").attr("src", "");
 	$("#exported").attr('href', "");
 	$("#aggexported").attr('href', "");
-			
-	date = $('#date').val();
-	date = new Date(date);
-	data = {date: date.toISOString()};
-	console.log(data);
-	$.ajax({
-		method: 'GET',
-		url: '/getURLsWithDate',
-		data: data,
-		success: function (result, status, xhr) {
-			$('#date').val(result.date);
-			$("#reportfile iframe").attr("src", result.report_url);
-			$("#imageplot img").attr("src", result.plot_url);
-			$("#exported").attr('href', result.exported_data_url);
-			$("#aggexported").attr('href', result.agg_exported_data_url);
-			console.log(result);
-		}
-	});
+	
+	//~ check if date is not null
+	if (date != "") {
+		//~ disable submit button and display info message
+		$("#datesubmit").attr('disabled', true);
+		$("#info").text("please wait, exporting data.");
+		//~ convert str to date to send to backend
+		date = new Date(date);
+		data = {date: date.toISOString()};
+		//~ ajax call to get URLs and download links from backend
+		$.ajax({
+			method: 'GET',
+			url: '/getURLsWithDate',
+			data: data,
+			success: function (result, status, xhr) {
+				$('#date').val(result.date);
+				$("#reportfile iframe").attr("src", result.report_url);
+				$("#imageplot img").attr("src", result.plot_url);
+				$("#exported").attr('href', result.exported_data_url);
+				$("#aggexported").attr('href', result.agg_exported_data_url);
+				//~ reenable submit button and clear info text
+				$("#info").text("");
+				$("#datesubmit").attr('disabled', false);
+			}
+		});
+	}
 }
 
 $(document).ready(function() {
+	//~ empty all fields when document loaded
 	$('#date').val("");
 	$("#reportfile iframe").attr("src", "");
 	$("#imageplot img").attr("src", "");
